5ecaa103bfbefcf36e8bf62ba301fc50a1d157af
Kimberly-Henderson <kimmiehenderson94@gmail.com>
2020-01-05 12:14:24 -0500

    Adding the Code War Solutions Project

--- CodeWarSolutions/src/main/java/math_solutions/DivisibleNb.java
+++ CodeWarSolutions/src/main/java/math_solutions/DivisibleNb.java
@@ -7,6 +7,6 @@ public class DivisibleNb {
 	}
 	
 	public static boolean isDivisible(long n, long x, long y) {
-		return (n % x == 0) && (n % y == 0) ? true : false;
+		return (n % x == 0) && (n % y == 0);
 	}
 }
